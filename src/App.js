da6ddfc42ea363abeed5727a2906f382f2613ed6
Nitin <nitin8285824551@gmail.com>
2023-04-30 11:18:01 +0530

    Todo app with local Storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([{}]);
+  const [value, setValue] = useState('');
+  const [index, setIndex] = useState();
+  const [checked, setChecked] = useState(false);
+
+  const changeValue = (e) => {
+    setValue(e.target.value)
+  }
+
+  const changeCheck = (e) => {
+    console.log(e.target.checked)
+    setChecked(e.target.checked)
+  }
+
+  useEffect(()=>{
+    const localData = localStorage.getItem('Todos');
+    if (localData !== null){
+      setTodos(JSON.parse(localData));
+    }
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('Todos', JSON.stringify(todos))
+  }, [todos])
+
+  const addTodo = ()=>{
+    if (index >= 0 && value){
+      const updateTodos = [...todos];
+      updateTodos[index].text = value;
+      updateTodos[index].checked = checked;
+
+      setTodos(updateTodos);
+      setValue('');
+      setChecked(false);
+      setIndex();
+    }else if (value){
+      setTodos(prevTodos => {
+        const newTodo = {text: value, checked: checked};
+        return [...prevTodos, newTodo];
+      })
+      setValue('');
+    }
+  }
+
+  const editTodo = (index) => {
+    const updateTodos = [...todos];
+    const updateValue = updateTodos[index].text;
+    setValue(updateValue);
+    setIndex(index);
+    setChecked(updateTodos[index].checked);
+  }
+
+  const deleteTodo = (index) => {
+    const updateTodos = [...todos];
+    updateTodos.splice(index, 1);
+    setTodos(updateTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos</h1>
+      <input type="text" name="" id="" value={value} onChange={changeValue}/>
+      <input type="checkbox" checked={checked} onChange={changeCheck} />
+      <button onClick={addTodo}>{index ? "Update" : "Add Todo"}</button>
+      <ul> 
+        {todos.map((todo, index) => (
+          <li key={index} onDoubleClick={() => {editTodo(index)}}>
+            <p className='todo-text' style = {{ textDecoration : todo.checked ? "line-through" : "none"}}>{todo.text}</p>
+            {/* <input type="button" value="Edit" onClick={() => {editTodo(index)}} className='todo-button' /> */}
+            <input type="button" value="Delete" onClick={deleteTodo} className='todo-button' />
+          </li>
+          ))}
+      </ul>
     </div>
   );
 }
